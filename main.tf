45271f4d55160d64c5424da0a8a9f567f5fdec48
Olatunji <adeteyelinux@gmail.com>
2024-07-05 19:56:51 +0100

    Set up GitHub Actions workflow for Terraform deployment using self-hosted runner

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0ff591da048329e00"
   instance_type = "t2.micro"
 
   tags = {
